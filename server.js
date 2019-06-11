40848386a7342dde1a0bc9ce5b5d780a19a3ebd7
forecubes <forecubes@Admins-MacBook-Pro.local>
2019-06-11 17:52:15 +0530

    server changes

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ const init = async () => {
         host: 'localhost'
     });
 
+    const setCookies = function(req,cookies){
+    	for (var i = 0; i < cookies.length; i++) {
+    		let cDetails = cookies[i].split(';')[0],rawCookie=cDetails.split('=');
+    		console.log('rawCookierawCookierawCookierawCookie',rawCookie);
+    		req.state(rawCookie[0],rawCookie[1],{ firstVisit: false });
+    	}
+    	return req
+    }
    
 	try{
 		await server.register(H2o2);
@@ -39,24 +47,24 @@ const init = async () => {
 		  		delete reqHeaders.pragma;
 		  		reqHeaders['cache-control'] = 'max-age=0';
 		  		reqHeaders['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3';
-		  		console.log('url------',url);
-		  		console.log(reqHeaders);
-		  		console.log('reqHeaders.cookie',req);
+		  		// console.log('url------',url);
+		  		// console.log(reqHeaders);
+		  		console.log('reqHeaders.cookie',req.state);
 		  		if(req.method==='get'){
 		  			var res = await Wreck.get(url,{headers:{cookie:reqHeaders.cookie||""},'content-type': 'application/json'});
-		  			console.log('res cookies-----',res.res.headers['set-cookie'])
-		  		 finalRes =  h.response(res.payload).header('set-cookie',res.res.headers['set-cookie']).header('content-type','application/json').header('Access-Control-Allow-Origin','*');
+		  			setCookies(h,res.res.headers['set-cookie']);
+		  		 finalRes =  h.response(res.payload).header('content-type','application/json').header('Access-Control-Allow-Origin','*');
 		  		}else if(req.method==='post'){
 		  			var res = await Wreck.post( url, {payload:req.payload,headers:{cookie:reqHeaders.cookie||""},'content-type': 'application/json'});
-		  			console.log('res cookies-----',res.res.headers['set-cookie'])
-		  		 finalRes =  h.response(res.payload).header('set-cookie',res.res.headers['set-cookie']).header('content-type','application/json').header('Access-Control-Allow-Origin','*');
+		  			setCookies(h,res.res.headers['set-cookie']);
+		  		 finalRes =  h.response(res.payload).header('content-type','application/json').header('Access-Control-Allow-Origin','*');
 		  		}else if(req.method==='OPTIONS' || req.method==='options'){
 		  			finalRes = h.response().header('Access-Control-Allow-Origin','*')
 		  									.header('Access-Control-Allow-Headers','cache-control,content-type')
 		  									.header('Access-Control-Allow-Method',req.method)
 		  									.header('Access-Control-Expose-Headers','access-control-allow-origin,access-control-allow-methods,access-control-allow-headers')
 		  									.header('Connection','keep-alive')
-		  									.header('Server','test')
+		  									.header('Server','localhost')
 		  									.header('Transfer-Encoding','chunked');
 		  		}
 		  		
